93fe0e25c330f064deb033b60d87e510c4a2db5e
bporcel <codamming@gmail.com>
2021-12-15 00:07:30 +0100

    Roles

--- JUEGO.py
+++ JUEGO.py
@@ -1,13 +1,12 @@
+import random
 from MESA import Mesa
 from JUGADOR import Jugador
 from Utilidades import Utilidades
-import itertools
 
 class Juego:
     CARTAS_INICIALES = 2
-    NUMERO_ROLES = 3
     FASES = {0: "inicial", 1: "apuestas", 2: "flop"}
-    #  ROLES = ["Botón", "Ciega Pequeña", "Ciega Grande"]
+    ROLES = {"boton": "Botón", "ciega_peque": "Ciega Pequeña", "ciega_grande": "Ciega Grande"}
 
     def __init__(self):
         self.mesa = Mesa()
@@ -19,17 +18,44 @@ class Juego:
     def orquestar(self):
         self.inicio_partida()
         self.orquestar_ronda()
-        self.orquestar_ronda()
+        # self.orquestar_ronda()
 
     def inicio_partida(self):
         self.mesa = Mesa()
         self.iniciar_jugadores()
         self.iniciar_roles()
 
+    def iniciar_jugadores(self):
+        num_jugadores = int(input("Introducir numero de jugadores: "))
+        for i in range(num_jugadores):
+            nombre = input("Nombre del jugador " + str(i + 1) + ": ")
+            jugador = Jugador(nombre)
+            self.jugadores.append(jugador)
+
+    def iniciar_roles(self):
+        print("Roles iniciales: ")
+        jugador_boton = self.asignarBotonAleatorio()
+        self.asignar_ciegas(jugador_boton)
+        self.imprimir_roles()
+
+    def asignarBotonAleatorio(self):
+        jugador = random.choice(self.jugadores)
+        jugador.asignar_rol(self.ROLES["boton"])
+        return jugador
+
+    def asignar_ciegas(self, jugador_boton):
+        indice_boton = self.jugadores.index(jugador_boton)
+        if len(self.jugadores) == 2:
+            jugador_boton.asignar_rol(self.ROLES["ciega_peque"])
+            self.jugadores[(indice_boton + 1) % len(self.jugadores)].asignar_rol(self.ROLES["ciega_grande"])
+        else:
+            self.jugadores[(indice_boton + 1) % len(self.jugadores)].asignar_rol(self.ROLES["ciega_peque"])
+            self.jugadores[(indice_boton + 2) % len(self.jugadores)].asignar_rol(self.ROLES["ciega_grande"])
+
     def orquestar_ronda(self):
         # contador_fase = 0
         self.fase_inicial()
-        self.fase_apuesta()
+        # self.fase_apuesta()
         # self.jugadores[2].fichas = 0
         # self.jugadores[0].fichas = 0
         # self.comprobar_actividad()
@@ -44,67 +70,30 @@ class Juego:
 
     def actualizar_roles(self):
         if self.contador_ronda != 1:
-
             print("Roles actualizados:")
-            self.set_dealer()
+            indice_boton = 0
+
             for jugador in self.jugadores:
-                jugador.asignar_rol(1, False)
-                jugador.asignar_rol(2, False)
-            # self.jugadores[2].fichas = 0
+                if self.ROLES["boton"] in jugador.roles:
+                    indice_boton = self.jugadores.index(jugador)
+                    
+                jugador.roles.clear()
+
+            nuevo_boton = self.jugadores[(indice_boton + 1) % len(self.jugadores)]
+            nuevo_boton.asignar_rol(self.ROLES["boton"])
             self.comprobar_actividad()
-            self.set_ciega_peque()
-            self.set_ciega_grande()
+            self.asignar_ciegas(nuevo_boton)
             self.imprimir_roles()
-        else:
-            pass
 
-    def set_dealer(self):
-        i = 0
-        for jugador in self.jugadores:
-            if jugador.rol[0]:  # Chequeo quien es el dealer, lo asigno al jugador +1 y pongo a false el anterior
-                self.jugadores[(i + 1) % len(self.jugadores)].asignar_rol(0, True)
-                self.jugadores[i].asignar_rol(0, False)
-                break
-            i += 1
-
-    def set_ciega_peque(self):
-        i = 0
-        for jugador in self.jugadores:
-            if jugador.rol[0]:  # Chequeo quien es el dealer, lo asigno al jugador +1 y pongo a false el anterior
-                self.jugadores[(i + 1) % len(self.jugadores)].asignar_rol(1, True)
-                break
-            i += 1
-
-    def set_ciega_grande(self):
-        i = 0
-        for jugador in self.jugadores:
-            if jugador.rol[0]:  # Chequeo quien es el dealer, lo asigno al jugador +1 y pongo a false el anterior
-                self.jugadores[(i + 2) % len(self.jugadores)].asignar_rol(2, True)
-            i += 1
-
-    def iniciar_jugadores(self):
-        num_jugadores = int(input("Introducir numero de jugadores: "))
-        for i in range(num_jugadores):
-            nombre = input("Nombre del jugador " + str(i + 1) + ": ")
-            jugador = Jugador(nombre)
-            self.jugadores.append(jugador)
 
     def comprobar_actividad(self):
+        self.jugadores[0].fichas = 0
         for jugador in self.jugadores:
             if jugador.comprueba_fichas():
+                if self.ROLES["boton"] in jugador.roles:
+                    indice_boton = self.jugadores.index(jugador)
+                    self.jugadores[(indice_boton + 1) % len(self.jugadores)]
                 self.jugadores.remove(jugador)
-
-    def iniciar_roles(self):
-        print("Roles iniciales: ")
-        if len(self.jugadores) == 2:
-            self.jugadores[0].asignar_rol(0, True)
-            self.jugadores[0].asignar_rol(1, True)
-            self.jugadores[1].asignar_rol(2, True)
-        else:
-            for i in range(self.NUMERO_ROLES):
-                self.jugadores[i].asignar_rol(i, True)
-
-        self.imprimir_roles()
 
     def repartir_cartas_iniciales(self):
         for _ in range(self.CARTAS_INICIALES):
@@ -124,11 +113,8 @@ class Juego:
         jugador.dibujar_mano()
 
     def imprimir_roles(self):
-        for i in range(len(self.jugadores)):
-            print(str(self.jugadores[i].nombre) + " " + str(self.jugadores[i].rol))
-
-
-
+        for jugador in self.jugadores:
+            print(jugador.nombre, jugador.roles)
 
 
     ''' def ronda(self):
--- JUGADOR.py
+++ JUGADOR.py
@@ -3,21 +3,28 @@ class Jugador(object):
     def __init__(self, nombre):
         self.nombre = nombre
         self.mano = []
-        self.rol = [False, False, False] # [Dealer, Ciega_Peque, Ciega_Grande]
+        self.roles = [] # [Dealer, Ciega_Peque, Ciega_Grande]
         self.fichas = 20
         self.activo = True
 
     def dibujar_mano(self):
         for carta in self.mano:
             carta.dibujar_carta()   # Duda como que podemos hacer ese dibujar_carta
-
-    def asignar_rol(self, rol, valor):
-        self.rol[rol] = valor
+        
+    def asignar_rol(self, rol):
+        self.roles.append(rol)
 
     def comprueba_fichas(self):
         if self.fichas == 0:
             return True
-        else:
-            return False
+        
+        return False
+
+    def esBoton(self):
+        if "Botón" in self.roles:
+            return True
+        
+        return False
+
 
 
