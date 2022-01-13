39f81092e6e9fd899404bfc842cf2fc3d119e949
bporcel <codamming@gmail.com>
2022-01-13 19:31:39 +0100

    Evaluador

--- EVALUADOR.py
+++ EVALUADOR.py
@@ -10,3 +10,27 @@ class Evaluador:
         # JUEGO.mesa.cartas_mesa
         # JUEGO.Jugador.mano = []
         # JUEGO.Jugador.dibujar_mano()
+
+
+        # isCombination() {
+        #     cartas_totales = [1, 2, 3, 4, 5, 6, 7]
+        #     isRoyalStair(cartas_totales)
+        #     jugador.asignPoints(9)
+
+
+
+        #     isPair(cartas_totales)
+        #     isThreesome()
+        #     isColor()
+        #     ...
+
+        # }
+
+        # sumHigherCard(cartas_jugador) {
+        #         mayor = cartas_jugador[0].value if cartas_jugador[0].value > cartas_jugador[1].value else cartas_jugador[1].value
+        #         totalPoints = 9 + mayor
+        # }
+
+        # compareTotalPoints(jugadores){
+
+        # }
